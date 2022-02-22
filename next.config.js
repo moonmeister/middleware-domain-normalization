dac4e733bbbaf25555be676672fa071dfbf56c43
Jordan Maslyn <jordan.maslyn@wpengine.com>
2022-02-22 13:10:18 -0500

    Use redirects instead of middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-const { withFaust } = require('@faustjs/next');
+const { withFaust } = require("@faustjs/next");
 
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withFaust();
+module.exports = withFaust({
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: ".*\\.js\\.wpenginepowered.com",
+          },
+        ],
+        permanent: true,
+        destination: `${process.env.NORMALIZED_URL}/:path*`,
+      },
+    ];
+  },
+});
