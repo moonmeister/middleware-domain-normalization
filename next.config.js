90ed73578bfa4822a0ed83fb150abc7a4d50e232
Jordan Maslyn <jordan.maslyn@wpengine.com>
2022-02-22 15:24:08 -0500

    Back to middleware

--- next.config.js
+++ next.config.js
@@ -3,20 +3,4 @@ const { withFaust } = require("@faustjs/next");
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withFaust({
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: ".*\\.js\\.wpenginepowered.com",
-          },
-        ],
-        permanent: true,
-        destination: `${process.env.NORMALIZED_URL}/:path*`,
-      },
-    ];
-  },
-});
+module.exports = withFaust();
---
+++ src/pages/_middleware.ts
@@ -0,0 +1,37 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const url = req.nextUrl.clone();
+
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isStaticFileRequest = url.pathname.includes(".");
+  const host = req.headers.get("host");
+
+  const normalizedHost = new URL(process.env.NORMALIZED_URL);
+  const isCorrectScheme = url.protocol === normalizedHost.protocol;
+  const isCorrectHostname = host.split(":")[0] === normalizedHost.hostname;
+  const isCorrectHost = isCorrectScheme && isCorrectHostname;
+
+  if (!isStaticFileRequest && !isDevelopment && !isCorrectHost) {
+    url.protocol = normalizedHost.protocol;
+    url.hostname = normalizedHost.hostname;
+    return NextResponse.redirect(url);
+  }
+
+  if (isStaticFileRequest) {
+    console.log(`Not redirecting because is static file: ${url.pathname}`);
+    return;
+  }
+
+  if (isDevelopment) {
+    console.log(`Not redirecting because NODE_ENV === ${process.env.NODE_ENV}`);
+    return;
+  }
+
+  if (isCorrectHostname) {
+    console.log(
+      `Not redirecting because hostname is already "${url.hostname}"`
+    );
+    return;
+  }
+}
