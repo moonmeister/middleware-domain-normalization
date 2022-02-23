0a5092b5aee33237a0399e1df78751e6875596dc
Jordan Maslyn <jordan.maslyn@wpengine.com>
2022-02-23 09:46:22 -0500

    Only throw error in prod and consolidate early return

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (!process.env.NORMALIZED_URL) {
-    console.error(
-      "You must provide a NORMALIZED_URL environment variable for the domain normalization middleware to work correctly"
-    );
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const hasEnvVariable = !process.env.NORMALIZED_URL;
+
+  if (isDevelopment || !hasEnvVariable) {
+    !hasEnvVariable &&
+      console.error(
+        "You must provide a NORMALIZED_URL environment variable for the domain normalization middleware to work correctly"
+      );
     return NextResponse.next();
   }
 
@@ -12,10 +16,9 @@ export function middleware(req: NextRequest) {
   const normalizedHost = new URL(process.env.NORMALIZED_URL);
   const host = req.headers.get("host");
 
-  const isDevelopment = process.env.NODE_ENV === "development";
   const isCorrectHostname = host.split(":")[0] === normalizedHost.hostname;
 
-  if (!isDevelopment && !isCorrectHostname) {
+  if (!isCorrectHostname) {
     url.protocol = normalizedHost.protocol;
     url.host = normalizedHost.host;
     url.port = normalizedHost.port;
